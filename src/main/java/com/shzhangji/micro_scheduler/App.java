ded868d767957e9fe5d761f42c681b8ac1da6f52
Ji Zhang <jizhang@anjuke.com>
2013-10-02 13:44:05 +0800

    init

---
+++ src/main/java/com/shzhangji/micro_scheduler/App.java
@@ -0,0 +1,13 @@
+package com.shzhangji.micro_scheduler;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
