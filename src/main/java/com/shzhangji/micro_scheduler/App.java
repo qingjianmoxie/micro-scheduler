4d7308e2b9880a607b2cf8bbe8a3d5e338946832
Ji Zhang <jizhang@anjuke.com>
2013-10-03 15:10:00 +0800

    workable

--- src/main/java/com/shzhangji/micro_scheduler/App.java
+++ src/main/java/com/shzhangji/micro_scheduler/App.java
@@ -1,18 +1,47 @@
 package com.shzhangji.micro_scheduler;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.Callable;
+import java.util.concurrent.Executors;
+
+import com.google.common.util.concurrent.AsyncFunction;
+import com.google.common.util.concurrent.Futures;
+import com.google.common.util.concurrent.ListenableFuture;
+import com.google.common.util.concurrent.ListeningExecutorService;
+import com.google.common.util.concurrent.MoreExecutors;
 
 public class App {
 
     public static void main(String[] args) throws Exception {
 
-        Topology topology = new Topology();
-        topology.addNode("A", new SqlTask("A"));
-        topology.addNode("B", new SqlTask("B"), "A");
-        topology.addNode("C", new SqlTask("C"));
-        topology.addNode("D", new SqlTask("D"), "B", "C");
-        topology.start();
-        topology.waitForCompletion();
+        final ListeningExecutorService executor = MoreExecutors.listeningDecorator(Executors.newCachedThreadPool());
+
+        ListenableFuture<Void> futureA = executor.submit(new SqlTask("A"));
+        ListenableFuture<Void> futureB = Futures.transform(futureA, new AsyncFunction<Void, Void>() {
+
+            @Override
+            public ListenableFuture<Void> apply(Void input) throws Exception {
+                return executor.submit(new SqlTask("B"));
+            }
+
+        }, executor);
+        ListenableFuture<Void> futureC = executor.submit(new SqlTask("C"));
+
+        List<ListenableFuture<Void>> futureBC = new ArrayList<ListenableFuture<Void>>();
+        futureBC.add(futureB);
+        futureBC.add(futureC);
+        ListenableFuture<Void> futureD = Futures.transform(Futures.allAsList(futureBC), new AsyncFunction<List<Void>, Void>() {
+
+            @Override
+            public ListenableFuture<Void> apply(List<Void> input)
+                    throws Exception {
+                return executor.submit(new SqlTask("D"));
+            }
+
+        }, executor);
+        futureD.get();
+        executor.shutdown();
 
     }
 
